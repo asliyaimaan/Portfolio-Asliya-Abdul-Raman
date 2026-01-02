32985dd283cd63c9e9321873d948525d42d86a59
asliyaimaan <imaan0415@gmail.com>
2026-01-02 12:45:47 +0400

    Added swiper

---
+++ swiper.js
@@ -0,0 +1,26 @@
+const swiper = new Swiper('.project-swiper', {
+  slidesPerView: 3,
+  spaceBetween: 40,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
